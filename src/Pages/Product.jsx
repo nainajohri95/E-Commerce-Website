69eb9c3106bad5db896f79df52b78ed603ea8280
Naina Johri <jhorinaina8@gmail.com>
2024-01-06 22:25:40 +0530

    Product.jsx updated

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
   return <div></div>;
 };
 
