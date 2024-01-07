ce9f4f038020813fb8f90fa04d77761f9126b6df
Naina Johri <jhorinaina8@gmail.com>
2024-01-07 23:35:33 +0530

    Breadcrum folder is created

---
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Breadcrum = () => {
+  return <div className="breadcrum"></div>;
+};
+
+export default Breadcrum;
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 const Product = () => {
   const { all_product } = useContext(ShopContext);
   const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
   return <div></div>;
 };
 
