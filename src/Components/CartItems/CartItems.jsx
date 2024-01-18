6c6708a10a99f4a9ad1a75d9cefb51beb21f465d
Naina Johri <jhorinaina8@gmail.com>
2024-01-18 18:42:32 +0530

    CartItems.jsx updated

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,10 +1,50 @@
 import React, { useContext } from "react";
 import "../CartItems.css";
 import { ShopContext } from "../../Context/ShopContext";
+import remove_icon from "../Assets/cart_cross_icon.png";
 
 const CartItems = () => {
   const { all_product, CartItems, removeFromCart } = useContext(ShopContext);
-  return <div className="cartitems"></div>;
+  return (
+    <div className="cartitems">
+      <div className="cartItems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+
+      {all_product.map((e) => {
+        if (CartItems[e.id] > 0) {
+          //means product is already available
+          return (
+            <div>
+              <div className="cartitems-format">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cartitems-quantity">
+                  {CartItems[e.id]}
+                </button>
+                <p>{e.new_price * CartItems[e.id]}</p>
+                <img
+                  src={remove_icon}
+                  onClick={() => {
+                    removeFromCart();
+                  }}
+                  alt=""
+                />
+              </div>
+              <hr />
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
 };
 
 export default CartItems;
