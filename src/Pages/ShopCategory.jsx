fc0de2779101854c2337eb3fe8282af76338d885
Naina Johri <jhorinaina8@gmail.com>
2024-01-21 15:32:17 +0530

    responsiveness added in Item.css

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import "./CSS/ShopCategory.css";
 import { ShopContext } from "../Context/ShopContext";
-import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import dropdown_icon from "../Components/Assets/dropdownIcon2.png";
 import Item from "../Components/Item/Item";
 const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
